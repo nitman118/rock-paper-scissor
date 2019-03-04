7c669393571a59182b33edcf9e9ee0bf60edcb2d
Nitish Singh <nitman118@gmail.com>
2019-03-04 02:07:02 +0100

    slight mod

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div=document.querySelector('.score-board');
 const result_div = document.querySelector('.result>p');
+result_div.innerHTML='';
 const rock_div=document.getElementById('r');
 const paper_div=document.getElementById('p');
 const scissor_div=document.getElementById('s');
